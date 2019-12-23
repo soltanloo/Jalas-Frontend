a2a5e2378a7085cafedea6af6807a7fdfb58d4f8
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-12-23 22:45:07 +0330

    Prive redux store to test App component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import reducers from '../reducers';
+
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(thunk))(createStore);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
