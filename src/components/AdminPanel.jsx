e73bf57ec96094eee8a02300ab3953cf0bc151dd
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-12-28 23:40:09 +0330

    Hotfix for panels

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchMetrics } from '../actions/analytic_actions';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 class AdminPanel extends Component {
   componentDidMount() {
@@ -8,16 +10,35 @@ class AdminPanel extends Component {
   }
 
   render() {
+    const {
+      reservedRoomsNum,
+      usersNum,
+      meetingsNum,
+      pollsNum,
+      canceledMeetingsNum,
+      commentsNum,
+    } = this.props.data;
     return (
-      <div>
-        {Object.keys(this.props.data).map((obj, i) => {
-          return (
-            <div>
-              {obj}: {this.props.data[obj]}
-            </div>
-          )})
-        }
-      </div>
+      <Paper style={{ padding: 10 }}>
+        <Typography>
+          تعداد کاربران: {usersNum}
+        </Typography>
+        <Typography>
+          تعداد جلسات ایجادشده: {meetingsNum}
+        </Typography>
+        <Typography>
+          تعداد نظرسنجی‌های ایجادشده: {pollsNum}
+        </Typography>
+        <Typography>
+          تعداد نظرها: {commentsNum}
+        </Typography>
+        <Typography>
+          تعداد جلسات لغوشده: {canceledMeetingsNum}
+        </Typography>
+        <Typography>
+          تعداد اتاق‌های رزروشده: {reservedRoomsNum}
+        </Typography>
+      </Paper>
     );
   }
 }
--- src/components/PerformancePanel.jsx
+++ src/components/PerformancePanel.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPerformance } from '../actions/analytic_actions';
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
 
 class PerformancePanel extends Component {
   componentDidMount() {
@@ -8,16 +10,27 @@ class PerformancePanel extends Component {
   }
 
   render() {
+    const {
+      meetingsPerPoll,
+      meetingsNum,
+      pollsNum,
+      creationMeanTime,
+    } = this.props.data;
     return (
-      <div>
-        {this.props.data && Object.keys(this.props.data).map((obj, i) => {
-          return (
-            <div>
-              {obj}: {this.props.data[obj]}
-            </div>
-          )})
-        }
-      </div>
+      <Paper style={{ padding: 10 }}>
+        <Typography>
+          نرخ ایجاد جلسه به‌ازای تعداد نظرسنجی‌ها: {meetingsPerPoll}
+        </Typography>
+        <Typography>
+          تعداد جلسات ایجادشده: {meetingsNum}
+        </Typography>
+        <Typography>
+          تعداد نظرسنجی‌های ایجادشده: {pollsNum}
+        </Typography>
+        <Typography>
+          میانگین زمان ایجاد جلسه و نظرسنجی‌: {creationMeanTime} میلی‌ثانیه
+        </Typography>
+      </Paper>
     );
   }
 }
