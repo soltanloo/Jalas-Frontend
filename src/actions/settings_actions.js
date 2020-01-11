92cbcdd21ae98671edd0ffc21e26b97efce6c675
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-01-11 20:39:12 +0330

    Remove redundant code

--- src/actions/settings_actions.js
+++ src/actions/settings_actions.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import Config from '../config/config';
 
-const MODIFY_REQUEST = 'MODIFY_REQUEST';
-const MODIFY_FAILURE = 'MODIFY_FAILURE';
-const MODIFY_SUCCESS = 'MODIFY_SUCCESS';
-
 export function modifyOption(optionName, isSet, cb) {
   return (dispatch) => {
     axios.post(`${Config.baseURL}/notification/${optionName}?notifyOn=${isSet}`, { notifyOn: isSet })
